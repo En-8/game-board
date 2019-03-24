f87ac13369d9dca4f7610027e5478e509511a1d9
Nate <nelliott2@madisoncollege.edu>
2019-03-24 14:24:53 +0000

    Installed CodeIgniter framework. Set base url, setup Pages controller, and set default controller to pages/view. Also added an (:any) route to pages/view/$1.

---
+++ application/views/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GameBoard -- <?=$title?></title>
+    </head>
+    
+    <body>
+        <nav>
+            <ul>
+                <li><a href="/">Logo Placeholder</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+        
